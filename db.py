b0795b808aba3057f419c633062803bfb41c5533
John Stoveld <jstoveld@gmail.com>
2024-09-30 20:25:00 -0400

    SQLAlchemy to stand up our db.

---
+++ db.py
@@ -0,0 +1,9 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+DATABASEURL = "postgrequl://admin:admn@localhost/shortydb"
+
+engine = create_engine(DATABASEURL)
+SessionLocal = sessionmaker(autocommut=False, autoflush=False, bind=engine)
+Base = declarative_base()
