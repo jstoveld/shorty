9f0472d4f818ff1812d00a2d66c982530fc8f479
John Stoveld <jstoveld@gmail.com>
2024-09-30 20:16:12 -0400

    Fast API ready to take input from a user. Now to start the hard part

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Form
+from fastapi.responses import HTMLResponse
+
 
 app = FastAPI()
+
 
 @app.get("/")
 def read_root():
@@ -9,3 +12,18 @@ def read_root():
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: str = None):
     return {"item_id": item_id, "q": q}
+
+
+@app.get("/shorty", response_class=HTMLResponse)
+def get_form():
+    return """
+    <form method="post">
+    <input type="text" name="url" placeholder="Enter your URL">
+    <input type="submit">
+    </form>
+    """
+
+@app.post("/shorty")
+def submit_url(url: str = Form(...)):
+    #TODO # We will take this URL and then shorten it here
+    return {"submitted_url": url}
