2f4078d5946c3ff24c93a195c1d3fadfbb4d370b
John Stoveld <jstoveld@gmail.com>
2024-10-02 13:45:20 -0400

    Redirect works by pasting the shortedned URL into address bar tailing shorty/ - App works as hoped thus far.

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI, Form, HTTPException, Depends
-from fastapi.responses import HTMLResponse
+from fastapi.responses import HTMLResponse, RedirectResponse
 from sqlalchemy.orm import Session
 from urllib.parse import urlparse
 import string
@@ -25,6 +25,8 @@ def get_db():
 
 
 # Function to validate our input URL
+#TODO # Or we reply with a 400 response if the URL is not valid
+#TODO # Will need to validate the string to ensure there is nothing malicious happening.
 def validate_url(url: str) -> bool:
     parsed = urlparse(url)
     return all([parsed.scheme, parsed.netloc])
@@ -42,8 +44,6 @@ def generate_short_url(length: int = 6) -> str:
     return "".join(random.choice(characters) for _ in range(length))
 
 
-
-
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: str = None):
     return {"item_id": item_id, "q": q}
@@ -59,7 +59,7 @@ def get_form():
     </form>
     """
 
-
+#DONE # We will take this URL and then shorten it here
 ## Shorty app POST function *Requierment 3*
 @app.post("/shorty")
 def submit_url(url: str = Form(...), db: Session = Depends(get_db)):
@@ -74,16 +74,26 @@ def submit_url(url: str = Form(...), db: Session = Depends(get_db)):
 
     return {"original_url": url, "shortened_url": shortened_url}
 
+#TODO # Return a 201 response with the shortened URL
+#TODO # Endpoint should return a 200 OK status code with the original URL
+#TODO # 301 redirect original Long URL
+@app.get("/shorty/{shortened_url}")
+def redirect_url(shortened_url: str, db: Session = Depends(get_db)):
+    db_url = db.query(models.URL).filter(models.URL.shortened_url == shortened_url).first()
+    if db_url is None:
+        raise HTTPException(status_code=404, detail="URL not found")
+    return RedirectResponse(url=db_url.original_url)
+
 
 ## Our Actual App will be here where we will shorten the URL
 ## We need to understand how the request will come in, store the original URL and then return the shortened URL
-    #TODO # We will take this URL and then shorten it here
+    
 
-    #TODO # Return a 201 response with the shortened URL
-    #TODO # Or we reply with a 400 response if the URL is not valid
-    #TODO # Endpoint should return a 200 OK status code with the original URL
-    #TODO # 301 redirect original Long URL
+
+
+
+
     #TODO # Update an existing short URL using a PUT method
-    #TODO # Will need to validate the string to ensure there is nothing malicious happening.
+
     #TODO # Return a 204 response if the URL is successfully deleted
     #TODO # 200 OK code with the statustics of the URL (IE AccessCount: 10)
